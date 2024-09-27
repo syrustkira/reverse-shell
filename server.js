068a127c52d54e678441565728e608980f5d95de
Blake Balick-Schreiber <blake.balick.schreiber@gmail.com>
2024-09-26 20:10:49 -0400

    Update server.js
    Update server to use express

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
+'use strict';
+const express =require('express')
+const app = express();
+const port = 8080;
+const handler = require('./api/index');
 
+app.get('/q', handler);
 
-const server = http.createServer((req, res) => {
-        if (/^\/https:\/\/[a-zA-Z0-9]{10}\.execute-api\.us-east-1\.amazonaws\.com:8443$/.test(req.url)) {
-                console.log(`Received request for: ${req.url}`);
-                            const filePath = path.join(__dirname, 'api/index.js');
-                            fs.readFile(filePath, (err, content) => {
-                                                if (err) {
-                                                                        res.writeHead(500, { 'Content-Type': 'text/plain' });
-                                                                        res.end('Internal Server Error');
-                                                                    } else {
-                                                                                            res.writeHead(200, { 'Content-Type': 'application/javascript' });
-                                                                                            res.end(content, 'utf-8');
-                                                                                        }
-                                            });
-                        } else {
-                                        res.writeHead(404, { 'Content-Type': 'text/plain' });
-                                        res.end('File Not Found');
-                                    }
-});
-
-const PORT = 8080;
-server.listen(PORT, '0.0.0.0', () => {
-     console.log(`Server is running at http://0.0.0.0:${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+console.log(`Server is running at http://0.0.0.0:${port}`);
 });
